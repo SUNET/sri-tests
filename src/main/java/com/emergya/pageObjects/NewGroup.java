716ca8d6f3a1fb95cc651807fc4afd605d7bd052
aoviedo <aoviedo@emergya.com>
2020-05-14 13:48:11 +0200

    =Test for groups added

--- src/main/java/com/emergya/pageObjects/NewGroup.java
+++ src/main/java/com/emergya/pageObjects/NewGroup.java
@@ -1,5 +1,10 @@
 package com.emergya.pageObjects;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.NoSuchElementException;
+
 import org.apache.log4j.Logger;
 import com.emergya.selenium.pageObject.*;
 
@@ -37,8 +42,12 @@ public class NewGroup extends BasePageObject {
     private static final String  SAVEBUTTON = "saveButton";
     private static final String  EDITBUTTON = "editGroup";
     private static final String  SENDBUTTON = "clickSend";
+    private static final String  CONTACTROW = "contactRow";
+    private static final String  CONTACTROW2 = "contactRow2";
     private static final String  DELETEBUTTON = "clickDelete";
-
+    private static final String  WORKLOGCOMMENT = "worklogComment";
+    private static String  CONTACTROWDIV = "div";
+    
     /**
      * Constructor method
      * @param driver selenium webdriver
@@ -68,6 +77,18 @@ public class NewGroup extends BasePageObject {
                 + " - End setName method");
     }
     
+    public String getName() {
+    	log.info("[log-PageObjects] " + this.getClass().getSimpleName()
+                + " - Start getName method");
+    	String name = "";
+    	if(this.isElementVisibleByXPath(NEWGROUP)) {
+    		name = this.getElementByXPath(NEWGROUP).getAttribute("value");
+    	}
+    	log.info("[log-PageObjects] " + this.getClass().getSimpleName()
+               + " - End getName method");
+    	return name;
+   }
+    
     public void setDescription(String description) {
     	log.info("[log-PageObjects] " + this.getClass().getSimpleName()
                 + " - Start setDescription method");
@@ -76,6 +97,18 @@ public class NewGroup extends BasePageObject {
     	}
     	log.info("[log-PageObjects] " + this.getClass().getSimpleName()
                + " - End setDescription method");
+    }
+    
+    public String getDescription() {
+    	log.info("[log-PageObjects] " + this.getClass().getSimpleName()
+                + " - Start getDescription method");
+    	String description = "";
+    	if(isDescriptionTexstAreaVisible()) {
+    		description = this.getElementByXPath(DESCRIPTIONTEXTAREA).getAttribute("value");
+    	}
+    	log.info("[log-PageObjects] " + this.getClass().getSimpleName()
+               + " - End getDescription method");
+    	return description;
     }
     
     public void setSearchForContact(String contact) {
@@ -175,6 +208,83 @@ public class NewGroup extends BasePageObject {
                + " - End clickDelete method");
     }
     
+    public WebElement getContactRowN(Integer n) {
+    	log.info("[log-PageObjects] " + this.getClass().getSimpleName()
+                + " - Start getContactRow method");
+    	WebElement contactRow = null;
+    	if(this.isElementVisibleByXPath(CONTACTROW)) {
+    		contactRow = this.getElementsByXPath(CONTACTROW).get(n);
+    	}
+    	
+    	if(this.isElementVisibleByXPath(CONTACTROW2)) {
+    		contactRow = this.getElementsByXPath(CONTACTROW2).get(n);
+    	}
+    	log.info("[log-PageObjects] " + this.getClass().getSimpleName()
+               + " - End getContactRow method");
+    	return contactRow;
+    }
+    
+    public String getContactNameN(Integer n) {
+    	log.info("[log-PageObjects] " + this.getClass().getSimpleName()
+                + " - Start getContactRow method");
+    	WebElement contactRow = this.getContactRowN(n);
+    	WebElement col = contactRow.findElements(By.xpath(CONTACTROWDIV)).get(0);
+    	String name = col.getText();
+    	log.info("[log-PageObjects] " + this.getClass().getSimpleName()
+               + " - End getContactRow method");
+    	return name;
+    }
+    
+    public String getContactOrganizationN(Integer n) {
+    	log.info("[log-PageObjects] " + this.getClass().getSimpleName()
+                + " - Start getContactOrganizationN method");
+    	WebElement contactRow = this.getContactRowN(n);
+    	WebElement col = contactRow.findElements(By.xpath(CONTACTROWDIV)).get(1);
+    	String organization = col.getText();
+    	log.info("[log-PageObjects] " + this.getClass().getSimpleName()
+               + " - End getContactOrganizationN method");
+    	return organization;
+    }
+    
+    public String getContactWorklogN(Integer n) {
+    	log.info("[log-PageObjects] " + this.getClass().getSimpleName()
+                + " - Start getContactWorklogN method");
+    	String worklogComment = "";
+    	if(this.isElementVisibleByXPath(WORKLOGCOMMENT)) {
+    		worklogComment = this.getElementsByXPath(WORKLOGCOMMENT).get(n).getText();
+    		
+    	}
+    	log.info("[log-PageObjects] " + this.getClass().getSimpleName()
+               + " - End getContactWorklogN method");
+    	return worklogComment;
+    }
+    
+    public List<List<String>> getContactEmailsN(Integer n) {
+    	log.info("[log-PageObjects] " + this.getClass().getSimpleName()
+                + " - Start getContactEmailsN method");
+    	WebElement contactRow = this.getContactRowN(n);
+    	WebElement col = contactRow.findElements(By.xpath(CONTACTROWDIV)).get(2);
+    	List<String> mails = Arrays.asList(col.getText().split("\n"));
+    	List<List<String>> mails_list = new ArrayList<List<String>>();
+    	for(int i=0;i<mails.size()/2;i++) {
+    		List<String> mail = Arrays.asList(mails.get(i*2),mails.get((i*2)+1));
+    		mails_list.add(mail);
+    	}
+    	log.info("[log-PageObjects] " + this.getClass().getSimpleName()
+               + " - End getContactEmailsN method");
+    	return mails_list;
+    }
+    
+    public List<String> getContactPhoneN(Integer n) {
+    	log.info("[log-PageObjects] " + this.getClass().getSimpleName()
+                + " - Start getContactPhoneN method");
+    	WebElement contactRow = this.getContactRowN(n);
+    	WebElement col = contactRow.findElements(By.xpath(CONTACTROWDIV)).get(3);
+    	List<String> phones = Arrays.asList(col.getText().split("\n"));
+    	log.info("[log-PageObjects] " + this.getClass().getSimpleName()
+               + " - End getContactPhoneN method");
+    	return phones;
+    }
     
     public boolean isDescriptionTexstAreaVisible() {
         log.info("[log-pageObjects]" + this.getClass().getSimpleName()
@@ -254,5 +364,86 @@ public class NewGroup extends BasePageObject {
         log.info("[log-pageObjects]" + this.getClass().getSimpleName()
                 + "]- End isButtonDeteleVisible method");
         return this.isElementVisibleByXPath(DELETEBUTTON);
+    }
+    
+    public boolean equals(String name, String description, List<List<Object>> contacts, List<String> worklogs) {
+    	boolean result = this.getName().equals(name);
+    	if(!result) {
+    		return false;
+    	}
+    	result &= this.getDescription().equals(description);
+    	if(!result) {
+    		return false;
+    	}
+    	Integer n = 0;
+    	while(true) {
+    		try {
+        		List<Object> contact = contacts.get(n);
+        		result &= this.getContactNameN(n).equals(contact.get(0));
+        		if(!result) {
+        			return false;
+        		}
+        		result &= this.getContactOrganizationN(n).equals(contact.get(1));
+        		if(!result) {
+        			return false;
+        		}
+        		List<List<String>> j_mails = (List<List<String>>) contact.get(2);
+        		List<List<String>> v_mails = this.getContactEmailsN(n);
+        		result &= j_mails.size() == v_mails.size();
+        		if(!result) {
+        			return false;
+        		}
+        		for(int i = 0;i<j_mails.size();i++) {
+        			List<String> j_mail = j_mails.get(i);
+        			List<String> v_mail = v_mails.get(i);
+        			result &= j_mail.get(0).equals(v_mail.get(0));
+        			if(!result) {
+        				return false;
+        			}
+        			result &= j_mail.get(1).equals(v_mail.get(1));
+        			if(!result) {
+        				return false;
+        			}
+        		}
+        		List<String> j_phones = (List<String>) contact.get(3);
+        		List<String> v_phones = this.getContactPhoneN(n);
+        		result &= j_phones.size() == v_phones.size();
+        		if(!result) {
+        			return false;
+        		}
+        		for(int i=0;i<j_phones.size();i++) {
+        			String j_p = j_phones.get(i);
+        			String v_p = v_phones.get(i);
+        			result &= j_phones.get(i).equals(v_phones.get(i));
+        			if(!result) {
+        				return false;
+        			}
+        		}
+    			n++;
+    		}catch(Exception e) {
+    			break;
+    		}
+    	}
+		result &= n.equals(contacts.size());
+		if(!result) {
+			return false;
+		}
+		n = 0;
+    	while(true) {
+    		try {
+				result &= this.getContactWorklogN(n).equals(worklogs.get(n));
+				if(!result) {
+					return false;
+				}
+				n++;
+			}catch(Exception e) {
+				break;
+			}
+		}
+		result &= n.equals(worklogs.size());
+		if(!result) {
+			return false;
+		}
+    	return result;
     }
  }
